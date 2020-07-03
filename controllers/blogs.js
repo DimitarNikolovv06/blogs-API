2dc156d12316b43f811254adae94718b3b3de006
Dimitar Nikolov <dimitarnikolovv06@gmail.com>
2020-07-03 14:41:56 +0300

    post put blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -8,4 +8,21 @@ blogsRouter.get("/", (req, res) => {
     .catch((err) => logger.info(err));
 });
 
+blogsRouter.post("/", (req, res) => {
+  const newBlog = new Blog(req.body);
+
+  newBlog
+    .save()
+    .then((blog) => res.status(200).json(blog))
+    .catch((err) => logger.error(err));
+});
+
+blogsRouter.get("/:id", (req, res) => {
+  console.log(req.params.id);
+
+  Blog.find({ _id: req.params.id })
+    .then((result) => res.json(result))
+    .catch((err) => logger.error(err));
+});
+
 module.exports = blogsRouter;
