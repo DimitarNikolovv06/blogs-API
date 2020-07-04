65dcd99bf1e7f51f8b1f6cd078fd81f4c7f63dec
Dimitar Nikolov <dimitarnikolovv06@gmail.com>
2020-07-04 19:06:55 +0300

    id, post tests and refactoring operations to use async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,27 +2,29 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const logger = require("../utils/logger");
 
-blogsRouter.get("/", (req, res) => {
-  Blog.find({})
-    .then((b) => res.json(b))
-    .catch((err) => logger.info(err));
+//get all blogs
+blogsRouter.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+
+  res.json(allBlogs);
 });
 
-blogsRouter.post("/", (req, res) => {
+//create new blog
+blogsRouter.post("/", async (req, res) => {
   const newBlog = new Blog(req.body);
 
-  newBlog
-    .save()
-    .then((blog) => res.status(200).json(blog))
-    .catch((err) => logger.error(err));
+  const blogSaved = await newBlog.save();
+
+  res.status(200).json(blogSaved);
 });
 
-blogsRouter.get("/:id", (req, res) => {
-  console.log(req.params.id);
+//get single blog
+blogsRouter.get("/:id", async (req, res) => {
+  // console.log(req.params.id);
 
-  Blog.find({ _id: req.params.id })
-    .then((result) => res.json(result))
-    .catch((err) => logger.error(err));
+  const blog = await Blog.find({ _id: req.params.id });
+
+  res.json(blog);
 });
 
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -7,18 +7,39 @@ const logger = require("../utils/logger");
 
 const api = supertest(app);
 
-// beforeEach(async () => {
-//   await Blog.deleteMany({});
+beforeEach(async () => {
+  await Blog.deleteMany({});
 
-//   let blogObj = new Blog(initialBlogs[0]);
-//   await blogObj.save();
+  let blogObj = new Blog(initialBlogs[0]);
+  await blogObj.save();
 
-//   blogObj = new Blog(initialBlogs[1]);
-//   await blogObj.save();
-// });
+  blogObj = new Blog(initialBlogs[1]);
+  await blogObj.save();
+});
 
 test("Blogs", async () => {
-  await api.get("/api/blogs").expect(200);
+  const res = await api.get("/api/blogs");
+
+  expect(res.type).toBe("application/json");
+  expect(res.body).toHaveLength(2);
+});
+
+test("id defined", async () => {
+  const res = await api.get("/api/blogs");
+
+  res.body.forEach((b) => expect(b.id).toBeDefined());
+  //   expect(res.body[0].id).toBeDefined();
+});
+
+test("post blog", async () => {
+  const newBlog = new Blog({
+    title: "posts",
+    author: "me",
+    url: "gdfgdgs.com",
+    likes: 1,
+  });
+
+  await api.post("/api/blogs", newBlog).expect(200);
 });
 
 afterAll(() => {
